6f5827384e8da268736272761212ef93190fa6b5
Philip Taylor <phil@zea.live>
2022-01-20 13:57:30 -0500

    chore: fixed build issue after fix to events.

--- src/Utilities/BaseEvent.ts
+++ src/Utilities/BaseEvent.ts
@@ -1,8 +1,5 @@
-import { GLBaseViewport } from '../Renderer/GLBaseViewport'
-
 /** Class representing a BaseEvent. */
 class BaseEvent {
-  viewport: GLBaseViewport
   /**
    * Create an BaseEvent.
    */
