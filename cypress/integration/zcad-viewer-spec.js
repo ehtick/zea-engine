7a11ea4a538438a8e2e4893111b406756849fc12
phtaylor <phil@zea.live>
2022-01-31 13:39:39 -0500

    test: implemented more tests for zcad-viewer.  - Comitting sample data.

--- cypress/integration/zcad-viewer-spec.js
+++ cypress/integration/zcad-viewer-spec.js
@@ -19,10 +19,20 @@ describe('zcad-viewer-gear_box_final_asm', () => {
 
 describe('zcad-viewer-HC_SRO4', () => {
   it('Capture snapshots', () => {
-    cy.visit(`testing-e2e/zcad-viewer.html?zcad=data/HC_SRO4.zcad`)
+    cy.visit(`testing-e2e/zcad-viewer.html?zcad=data/zcad/3.9.1/HC_SRO4.zcad`)
 
     cy.get('#status').should('have.text', `done-loading`)
     const test = 'zcad-viewer-HC_SRO4'
     cy.get('canvas').percySnapshot('zcad-viewer-HC_SRO4')
+  })
+})
+
+describe('zcad-viewer-PressRink', () => {
+  it('Capture snapshots', () => {
+    cy.visit(`testing-e2e/zcad-viewer.html?zcad=data/zcad/3.9.1/PressRink.CATProduct.zcad`)
+
+    cy.get('#status').should('have.text', `done-loading`)
+    const test = 'zcad-viewer-PressRink'
+    cy.get('canvas').percySnapshot('zcad-viewer-PressRink')
   })
 })
