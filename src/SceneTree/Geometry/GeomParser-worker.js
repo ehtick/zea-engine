e2085c92b4f09215ca32f6cad2163bad698cc74a
phtaylor <phil@zea.live>
2022-01-31 13:08:58 -0500

    chore: added warning message when passing data to the webworker fails.

--- src/SceneTree/Geometry/GeomParser-worker.js
+++ src/SceneTree/Geometry/GeomParser-worker.js
@@ -1,6 +1,13 @@
 import { parseGeomsBinary } from './parseGeomsBinary'
 
 self.onmessage = function (event) {
+  if (!event.data) {
+    // Note: we see this occur when loading one large asset many times.
+    // Like when loading the portafil.
+    // It may be due to memory issues, but its not clear.
+    console.warn('GeomLibrary worker.postMessage failed. data was lost on the way to the web worker.')
+    return
+  }
   parseGeomsBinary(event.data, (data, transferables) => {
     self.postMessage(data, transferables)
   })
