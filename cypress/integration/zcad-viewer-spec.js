1e11d846ddfd8c3d6d645aed988a30ce9e22bee3
phtaylor <phil@zea.live>
2022-01-31 13:30:58 -0500

    test: implemented simple zcad-viewer test which will replace the engine-cad-ux-collab-potree test

---
+++ cypress/integration/zcad-viewer-spec.js
@@ -0,0 +1,28 @@
+describe('zcad-viewer-Dead_eye_bearing', () => {
+  it('Capture snapshots', () => {
+    cy.visit(`testing-e2e/zcad-viewer.html?zcad=data/zcad/3.9.1/Dead_eye_bearing.stp.zcad`)
+
+    cy.get('#status').should('have.text', `done-loading`)
+    const test = 'zcad-viewer'
+    cy.get('canvas').percySnapshot(test)
+  })
+})
+describe('zcad-viewer-gear_box_final_asm', () => {
+  it('Capture snapshots', () => {
+    cy.visit(`testing-e2e/zcad-viewer.html?zcad=data/zcad/3.9.1/gear_box_final_asm.zcad`)
+
+    cy.get('#status').should('have.text', `done-loading`)
+    const test = 'zcad-viewer-gear_box_final_asm'
+    cy.get('canvas').percySnapshot('zcad-viewer-gear_box_final_asm')
+  })
+})
+
+describe('zcad-viewer-HC_SRO4', () => {
+  it('Capture snapshots', () => {
+    cy.visit(`testing-e2e/zcad-viewer.html?zcad=data/HC_SRO4.zcad`)
+
+    cy.get('#status').should('have.text', `done-loading`)
+    const test = 'zcad-viewer-HC_SRO4'
+    cy.get('canvas').percySnapshot('zcad-viewer-HC_SRO4')
+  })
+})
---
+++ testing-e2e/src/zea-fps-display.js
@@ -0,0 +1,34 @@
+class FPSDisplay extends HTMLElement {
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({ mode: "open" });
+
+    this.div = document.createElement("div");
+    this.div.classList.add("fps-display");
+    shadowRoot.appendChild(this.div);
+
+    const styleTag = document.createElement("style");
+    styleTag.appendChild(
+      document.createTextNode(`
+
+    .fps-display {
+      color: black;
+    }
+`)
+    );
+    shadowRoot.appendChild(styleTag);
+  }
+
+  set renderer(renderer) {
+    let frameCounter = 0;
+    renderer.on("redrawOccurred", () => {
+      frameCounter++;
+    });
+    setInterval(() => {
+      this.div.textContent = `Fps: ${frameCounter * 2}`;
+      frameCounter = 0;
+    }, 500);
+  }
+}
+
+customElements.define("zea-fps-display", FPSDisplay);
