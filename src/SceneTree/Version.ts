5448cffa901e9e025bfbab7e9f2f607aea529a49
phtaylor <phil@zea.live>
2022-05-24 12:15:45 -0400

    feat: Version class can now be constructed from a string, or an array, and can be converted back to an array for comparisons with other Version instances.

--- src/SceneTree/Version.ts
+++ src/SceneTree/Version.ts
@@ -2,29 +2,30 @@
  * Class designed to store version data. Widely used in the zea engine for backwards compatibility.
  */
 class Version {
-  major: number
-  minor: number
-  patch: number
+  major: number = 0
+  minor: number = 0
+  patch: number = 0
   branch: string = ''
   /**
    * Creates a version.
    * The version string should have the following structure:
    * major, minor and patch separated by a dot(`.`) and parts separated by a dash(`-`).
    *
-   * @param versionStr - The version string value.
+   * @param arg - The version string value, or an array of version numbers.
    */
-  constructor(versionStr: string = '') {
-    if (versionStr) {
-      const parts = versionStr.split('-')
+  constructor(arg?: string | Array<number>) {
+    if (typeof arg == 'string') {
+      const parts = arg.split('-')
       const numbers = parts[0].split('.')
       this.major = parseInt(numbers[0])
-      this.minor = parseInt(numbers[1])
-      this.patch = parseInt(numbers[2])
+      this.minor = numbers.length > 1 ? parseInt(numbers[1]) : 0
+      this.patch = numbers.length > 2 ? parseInt(numbers[2]) : 0
       if (parts.length == 2) this.branch = parts[1]
-    } else {
-      this.major = 0
-      this.minor = 0
-      this.patch = 0
+    } else if (Array.isArray(arg)) {
+      const numbers = arg
+      this.major = numbers[0]
+      this.minor = numbers.length > 1 ? numbers[1] : 0
+      this.patch = numbers.length > 2 ? numbers[2] : 0
     }
   }
 
@@ -42,6 +43,20 @@ class Version {
       if (v1[i] !== numbers[i]) return v1[i] - numbers[i]
     }
     return 0
+  }
+
+  /**
+   * Converts the Version class instance back to an array for comparisons with other version class instances.
+   * e.g.
+   * ```
+   *   const version1 = new Version([1, 2, 3])
+   *   const version2 = new Version([1, 2, 4])
+   *   const res = version1.compare(version2.asArray())
+   * ```
+   * @returns an array containing the major, minor and patch version numbers.
+   */
+  asArray() {
+    return [this.major, this.minor, this.patch]
   }
 
   toString(): string {
